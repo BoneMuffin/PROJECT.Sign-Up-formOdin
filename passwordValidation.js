cb5ee8c011fb0d93e7b05518e595fba570bbf58e
bonemuffin <101133295+BoneMuffin@users.noreply.github.com>
2022-12-26 12:55:22 -0800

    added js file for password validation

---
+++ passwordValidation.js
@@ -0,0 +1,28 @@
+// Function to check Whether both passwords
+    // is same or not.
+
+function checkPassword(form) {
+        password == form.password.value;
+        password2 == form.password2.value;
+
+    // If password not entered
+        if (password == '')
+        alert ("Please enter Password");
+
+        // If confirm password not entered
+        else if (password2 == '')
+        alert ("Please confirm password");
+    
+    // If Not same return False.    
+    else if (password != password2) {
+        alert ("\nPassword did not match: Please try again.")
+        return false;
+    }
+        
+    // If same return True.
+    else {
+        alert("Password Match: Welcome to Santa Puerca!")
+        return true;
+        }
+} 
+
